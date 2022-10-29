6d5e6fe80e7bee4c742b131b1e650d779af33c4a
Pau Tusell <ptapfel@gmail.com>
2022-10-29 11:06:37 +0200

    Develop branch created

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("MagicWars");
+
     }
     public ArrayList<Character> createPartiesRandom {
 
